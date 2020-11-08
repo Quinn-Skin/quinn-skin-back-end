170680c950372e6cfbb9009a90ca92bdfabf8a96
Brayhan De Aza <brayhandeaza@gmail.com>
2020-11-08 12:48:25 -0500

    production

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ let db
 const production = true
 
 if (production) {
-    db = new Sequelize("postgres://nmfxlkhtwrbgxw:c2281bba624684b99b6c94c875350fa8778147967371099ab7c8e3fc526211d0@ec2-54-158-222-248.compute-1.amazonaws.com:5432/d31i5i63v8bbtg")
+    db = new Sequelize("postgres://rixnhoryukwese:e1097a23504add7d0d7353e2f1d14b810a3d78c1c1d937b43637c78c0b3a4834@ec2-52-5-176-53.compute-1.amazonaws.com:5432/d9ji9jebnmdrj3")
 
 } else {
     db = new Sequelize({
