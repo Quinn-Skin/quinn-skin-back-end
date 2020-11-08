cbb3a478262fc40b39490bea32b6ed08e5662d11
Brayhan De Aza <brayhandeaza@gmail.com>
2020-11-08 14:08:39 -0500

    review models and reviews routes

--- seed.js
+++ seed.js
@@ -1,7 +1,10 @@
 const Products = require("./models/Produts")
+const Reviews = require("./models/Reviews")
 const data = require("./data")
+const faker = require('faker');
 
-// // console.log(data[84].image.slice(1, -1).split(",")[0].slice(0, -8) + "500_.jpg")
+
+// Seed Products
 for (let i = 0, j = 1; i < data.length; j++, i++) {
     Products.create({
         title: data[i].title,
@@ -9,8 +12,16 @@ for (let i = 0, j = 1; i < data.length; j++, i++) {
         url: data[i].image ? data[i].image.slice(1, -1).split(",")[0].slice(1, -8) + "500_.jpg" : 'https://images-na.ssl-images-amazon.com/images/I/21BJjd-RsvL._SS500_.jpg',
         price: data[i].price || 25.99
     })
-
-    // console.log(data[i].image, i)
 }
 
-// console.log(uniqueArray)
+
+// Seed Reviews
+for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < 8; j++) {
+        Reviews.create({
+            fullName: faker.name.findName(),
+            review: faker.lorem.sentence(9),
+            productId: i
+        })
+    }
+}
